fec337d92418862fa5f59b2e8f26b55d47e17579
thai <thaihd@athena.studio>
2024-11-13 17:40:38 +0700

    Continue developing GUI Part 6

--- Windows/competency_settings.py
+++ Windows/competency_settings.py
@@ -2,7 +2,7 @@ import customtkinter
 from customtkinter import CTkLabel
 import tkinter
 from Styles.styles import title_font, h1_font, button_font
-from Windows.components import InputSection
+from Windows.components import SettingsInputSection
 
 class TopLevelCompetencySettings(customtkinter.CTkToplevel):
     def __init__(self):
@@ -14,26 +14,5 @@ class TopLevelCompetencySettings(customtkinter.CTkToplevel):
         self.label.grid(row=0, column=0, padx=10, pady=10, sticky="nwe")
 
         # Input Category field
-        self.category_frame = InputSection(self, "Category")
+        self.category_frame = SettingsInputSection(self, "Category")
         self.category_frame.grid(row=1, column=0, padx=10, pady=10, sticky="nw")
-
-        # self.category_label = CTkLabel(self.category_frame, text="Category", font=h1_font, padx=5, pady=5)
-        # self.category_label.grid(row=0, column=0, padx=10, pady=10, sticky="nw")
-        #
-        # self.category_input = customtkinter.CTkEntry(self.category_frame, width=220, height=50)
-        # self.category_input.grid(row=1, column=0, padx=(10, 0), sticky="nw")
-        #
-        # self.category_add_btn = customtkinter.CTkButton(self.category_frame, text="Add", font=button_font, height=50, width=50, command=self.category_add_btn)
-        #
-        # self.category_add_btn.grid(row=1, column=1, padx=10, sticky="n")
-
-
-        self.category_list_frame = customtkinter.CTkScrollableFrame(self.category_frame, height=300, width=200)
-        self.category_list_frame.grid(row=2, column=0, padx=10, pady=10)
-
-        self.category_list = []
-
-    def category_add_btn(self, category_list=None):
-        category_data = self.category_input.get()
-        self.category_list.append(category_data)
-        self.category_input.delete(0, "end")
--- Windows/components.py
+++ Windows/components.py
@@ -2,7 +2,7 @@ import customtkinter
 from Styles.styles import h1_font, button_font
 from customtkinter import CTkLabel
 
-class InputSection(customtkinter.CTkFrame):
+class SettingsInputSection(customtkinter.CTkFrame):
     def __init__(self, master, input_field_name):
         super().__init__(master, fg_color="transparent")
         self.input_field_name = input_field_name
@@ -13,5 +13,21 @@ class InputSection(customtkinter.CTkFrame):
         self.category_input.grid(row=1, column=0, padx=(10, 0), sticky="nw")
 
         self.category_add_btn = customtkinter.CTkButton(self, text="Add", font=button_font, height=50,
-                                                        width=50)
+                                                        width=50, command=self.add_category)
         self.category_add_btn.grid(row=1, column=1, padx=10, sticky="n")
+
+        self.category_list_frame = customtkinter.CTkScrollableFrame(self, height=300, width=200)
+        self.category_list_frame.grid(row=2, column=0, padx=10, pady=10)
+
+        self.category_list = []
+
+    def add_category(self):
+        category_data = self.category_input.get()
+        self.category_list.append(category_data)
+        self.category_input.delete(0, "end")  # Clear the input field
+
+        for i, category in enumerate(self.category_list, start=1):
+            category_label = CTkLabel(self.category_list_frame, text=f"{i}. {category}")
+            category_label.grid(row=i, column=0, padx=10, pady=10, sticky="nw")
+
+
